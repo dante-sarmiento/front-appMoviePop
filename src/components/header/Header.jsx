107c99c2cfac8ebd95a50f7bf6dc664a184abd1e
dante-sarmiento <91224241+dante-sarmiento@users.noreply.github.com>
2022-05-16 13:37:39 -0300

    Movies (#1)
    * pagina Home y sus componentes. Login y registro con sus endpoints. carga de peliculas y sus funcionalidades.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react'
+import { faArrowRightFromBracket, faMagnifyingGlass, faStar } from '@fortawesome/free-solid-svg-icons'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './header.css'
 import { Menu } from 'antd'
+import { Navigate } from 'react-router-dom'
 
 
 export const Header = () => {
+
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("currentUser")));
+
+    const logout = ()=> {
+        console.log(logout)
+        localStorage.removeItem('userToken');
+        localStorage.removeItem('currentUser');
+        setUser(null);
+    }
     return (
         <>
-            <Menu>
-                <div className='containerHeader'>
-                    <a href="/"><FontAwesomeIcon icon={faHouse} className='icons' /></a>
-                    <a href="/movies"><FontAwesomeIcon icon={faMagnifyingGlass} className='icons' /></a>
-                </div>
-            </Menu>
-
-
-
-
+            <div >
+                <Menu className='containerHeader'>
+                    <a href="/*"><FontAwesomeIcon icon={faHouse} className='icons' /></a>
+                    <a href="/Movies"><FontAwesomeIcon icon={faMagnifyingGlass} className='icons' /></a>
+                    <a href="/Favorites"><FontAwesomeIcon icon={faStar} className='icons'/></a>
+                    <a href='/login' onClick={logout}><FontAwesomeIcon icon={faArrowRightFromBracket} className='icons' /></a>
+                </Menu>
+            </div>
         </>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,10 @@ import { Layout } from 'antd'
 import React from 'react'
 import { Header } from '../../components/header/Header'
 import { ContentHome } from '../../components/content/ContentHome'
-import { Route, Routes } from 'react-router-dom'
-import { MoviesGrid } from '../MoviesGrid/MoviesGrid'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { Movies } from '../Movies/Movies'
+import { MoviesDetails } from '../Movies/moviesDetails/MoviesDetails'
+import { FavoritesMovies } from '../Movies/moviesDetails/favoritesMovies/FavoritesMovies'
 
 
 export const Home = () => {
@@ -14,8 +16,10 @@ export const Home = () => {
                         <Header />
                     </header>
                         <Routes>
-                            <Route path="/" element={<ContentHome />}/>
-                            <Route path="/Movies" element={<MoviesGrid />}/>  
+                            <Route path="/*" element={<ContentHome />}/>
+                            <Route path="/Movies" element={<Movies />}/>
+                            <Route path="/MoviesDetails/:movieID" element={<MoviesDetails />}/>  
+                            <Route path='/Favorites' element={<FavoritesMovies />}/>
                         </Routes>
                 </Layout>
 
